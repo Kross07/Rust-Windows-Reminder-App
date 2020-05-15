bc88ff65262c124821ab54578d48d49ba3454f49
Kross07 <65046270+Kross07@users.noreply.github.com>
2020-05-14 21:16:01 -0700

    Create Reminder_App.rs

---
+++ Reminder_App.rs
@@ -0,0 +1,9 @@
+/*
+
+Begin Rust Code for Windows Notification Pop-Ups
+This project is to remind me to move around every 30-40 minutes randomly.
+
+This Pop-up will randomly select a physical activity from a list such as:
+Push-ups, sit-ups, jumping-jacks, walk around the block, jog around the block, etc.
+
+*/
